0965b4e337ccbd1a23a0fe4eaf26d8ff3445ae5b
hugosenari <hugosenari@gmail.com>
2022-03-12 00:20:09 -0300

    chore: re index alsa

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''NIM bindings for ALSA-LIB c library'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-alsa-master.flake = false;
+  inputs.src-alsa-master.owner = "eagledot";
+  inputs.src-alsa-master.ref   = "refs/heads/master";
+  inputs.src-alsa-master.repo  = "nim-alsa";
+  inputs.src-alsa-master.type  = "gitlab";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-alsa-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-alsa-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
